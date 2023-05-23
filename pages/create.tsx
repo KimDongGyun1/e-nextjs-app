82ed7def9de73b726234897db2a528f0cb123f09
KimDongGyun1 <potter1185@gmail.com>
2023-05-23 14:39:50 +0900

    first commit

---
+++ pages/create.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <Layout>
+        <h2>create</h2>
+        Hello, create
+      </Layout>
+    </>
+  );
+}
---
+++ pages/read/[id].tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <Layout>
+        <h2>Read</h2>
+        Hello, Read!
+      </Layout>
+    </>
+  );
+}
